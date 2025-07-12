bb4c2bf7f7f50e509426091f3cf4741e2b11bcd9
Gaff <no@email.please>
2025-07-12 22:58:23 +0000

    Setup TFE

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,5 @@
-from main import hello_world
+#from main import hello_world
 from photoclassifier.main import hello_gcf
-
-def test_hello_world():
-    request = None  # Simulate a request object (could be a mock)
-    response = hello_world(request)
-    assert response == "Hello World!"
 
 def test_hello_world2():
     request = None  # Simulate a request object (could be a mock)
