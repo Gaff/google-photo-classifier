6945b9125b40a811ae99771deaf926523fdcb007
Gaff <no@email.please>
2025-07-13 07:08:41 +0000

    Bootstrap tfe

--- infra/main.tf
+++ infra/main.tf
@@ -2,18 +2,26 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.0"
-    }
-    archive = {
-      source  = "hashicorp/archive"
-      version = "~> 2.2"
+      version = "~> 5.0"
     }
   }
+
+  backend "gcs" {
+    bucket = "${var.gcp_project}-terraform-state"
+    prefix = "terraform/state"
+  }
+
 }
 
 provider "google" {
   project = var.gcp_project
   region  = var.gcp_region
+}
+
+
+resource "google_compute_network" "vpc" {
+  name                    = "gpc-vpc"
+  auto_create_subnetworks = false
 }
 
 variable "gcp_project" {
