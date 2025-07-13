6e3351d80add65fe22b415911dbaff4a0a9ade6e
Gaff <no@email.please>
2025-07-13 08:31:25 +0000

    Bootstrap tfe

--- infra/bootstrap.sh
+++ infra/bootstrap.sh
@@ -19,6 +19,13 @@ gcloud storage buckets update gs://${BUCKET_NAME} --versioning
 
 # WIF: (didn't get it to work!)
 
+# Enable APIs:
+#    Identity and Access Management (IAM) API
+#    Cloud Resource Manager API
+#    IAM Service Account Credentials API
+#    Security Token Service API
+
+
 gcloud iam workload-identity-pools create "github-pool" \
     --project="${GOOGLE_CLOUD_PROJECT}" \
     --location="global" \
@@ -29,9 +36,6 @@ gcloud iam workload-identity-pools describe "github-pool" \
     --format="value(name)"
 
 gcloud iam workload-identity-pools providers create-oidc "github-provider" \
-  --project="${GOOGLE_CLOUD_PROJECT}" \
-  --location="global" \
-  --workload-identity-pool="github-pool" \
-  --display-name="GitHub Provider" \
-  --attribute-mapping="google.subject=assertion.sub" \
-  --issuer-uri="https://token.actions.githubusercontent.com"
+    --project="${GOOGLE_CLOUD_PROJECT}"   --location="global"   --workload-identity-pool="github-pool"   \
+    --display-name="GitHub Provider"   --issuer-uri="https://token.actions.githubusercontent.com"   \
+    --attribute-mapping="google.subject=assertion.sub" --attribute-condition="assertion.repository=='Gaff/google-photo-classifier'"
