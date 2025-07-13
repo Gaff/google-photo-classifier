86fd3017666849141e22b26895d316e59b5c58d1
Matthew <github@shaylor.co.uk>
2025-07-13 13:13:41 +0100

    Put main.tf back (#15)
    Co-authored-by: Gaff <no@email.please>

---
+++ infra/main.tf
@@ -0,0 +1,20 @@
+
+
+resource "google_storage_bucket" "code_bucket" {
+  name     = "${var.gcp_project}-code-bucket"
+  location = "${var.gcp_region}"
+  versioning {
+    enabled = true
+  }
+}
+
+resource "google_project_service" "pubsub" {
+  # Bit of a gotcha but requires a delay before it goes live!
+  project = var.gcp_project
+  service = "pubsub.googleapis.com"
+}
+
+resource "google_pubsub_topic" "test-topic" {
+  name = "my-test-topic"
+}
+
