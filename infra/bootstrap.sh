7c9c2a47a19e869e219524413ca095a03475c91a
Gaff <no@email.please>
2025-07-13 13:56:57 +0000

    Setup cloud funcs

--- infra/bootstrap.sh
+++ infra/bootstrap.sh
@@ -15,7 +15,7 @@ gcloud storage buckets update gs://${BUCKET_NAME} --versioning
 
 
 # I must have have create a terrform service account, with viewer role
-# It also needs: 
+# It also needs (for cloudrun functions): 
  gcloud projects add-iam-policy-binding ${GOOGLE_CLOUD_PROJECT} \
   --member="serviceAccount:terraform-pipeline@${GOOGLE_CLOUD_PROJECT}.iam.gserviceaccount.com" \
   --role="roles/run.admin"
